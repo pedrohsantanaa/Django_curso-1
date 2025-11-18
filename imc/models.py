fe480641cc2fbd912c5eb295507c7c1a80466fd9
furtadoismael15-byte <furtadoismael15@gmail.com>
2025-11-18 17:31:17 -0300

    versão 2
    2

--- imc/models.py
+++ imc/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+class Tarefas(models.Model):
+    peso = models.CharField(max_length=255)
+    altura = models.CharField(max_length=255)
+    calculo = models.TextField()
+    usuario = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.usuario
